f75f571649fdc95a1b84d14cc7525d403035328e
Ed Orsini <edorsini@mit.edu>
2017-06-16 13:56:30 -0400

    working on not picking from already selected students

--- js/main.js
+++ js/main.js
@@ -2,10 +2,31 @@
 
     var images = ['danny.png', 'kelsey.png', 'mehdi.png', 'pei.png', 'david.png', 'hongsyn.jpeg', 'kirk.jpeg', 'melissa.png', 'robert.jpeg', 'thomas.jpeg', 'daniel.png', 'don.png', 'jamie.jpeg', 'matthew.jpeg', 'michael.png', 'ross.png', 'zack.png'];
 
+    var pickedOn = [];
+
     $(document).ready(function() {
+        var index;
+        var student;
+        var count = 0;
+
         $("#ask").click(function() {
-            var index = Math.floor(Math.random() * students.length);
-            var student = students[index];
+            if (count === students.length) {
+                alert("You have picked on everyone today.  Please refresh the page to start again.");
+                return;
+            }
+
+            var pickedOnStudentAlreadyFlag = true;
+
+            while (pickedOnStudentAlreadyFlag === true && count < students.length) {
+                index = generateStudentIndex();
+                student = students[index];
+                console.log("student = " + student);
+                pickedOnStudentAlreadyFlag = pickedOnStudentAlready(student);
+
+            }
+
+            console.log(pickedOn);
+
             var image = images[index];
             $(".student-name").text(student);
             $(".student-name").removeClass('hide');
@@ -13,4 +34,22 @@
             $(".student-image").removeClass('hide');
             $(".thank-you").removeClass('hide');
         });
+
+
+        function generateStudentIndex() {
+            return Math.floor(Math.random() * students.length);
+        }
+
+        function pickedOnStudentAlready(student) {
+            if (pickedOn.includes(student)) {
+                console.log("found student!");
+                return true;
+            } else {
+                console.log("didn't find student");
+                pickedOn.push(student);
+                count++;
+                return false;
+            }
+        }
+
     });
