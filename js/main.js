55bd9fa7962c147af85aaec4891d626897b8e6ae
Ed Orsini <edorsini@mit.edu>
2017-07-12 19:49:14 -0400

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,20 +1,17 @@
-    var students = ['Danny', 'Kelsey', 'Mehdi', 'Pei', 'David', 'Hongsyn', 'Kirk', 'Melissa', 'Robert', 'Thomas', 'Daniel', 'Don', 'Jamie', 'Matthew', 'Michael', 'Ross', 'Zack'];
+    //var students = ['Danny', 'Kelsey', 'Mehdi', 'Pei', 'David', 'Hongsyn', 'Kirk', 'Melissa', 'Robert', 'Thomas', 'Daniel', 'Don', 'Jamie', 'Matthew', 'Michael', 'Ross', 'Zack'];
 
-    var images = ['danny.png', 'kelsey.png', 'mehdi.png', 'pei.png', 'david.png', 'hongsyn.jpeg', 'kirk.jpeg', 'melissa.png', 'robert.jpeg', 'thomas.jpeg', 'daniel.png', 'don.png', 'jamie.jpeg', 'matthew.jpeg', 'michael.png', 'ross.png', 'zack.png'];
+    //var images = ['danny.png', 'kelsey.png', 'mehdi.png', 'pei.png', 'david.png', 'hongsyn.jpeg', 'kirk.jpeg', 'melissa.png', 'robert.jpeg', 'thomas.jpeg', 'daniel.png', 'don.png', 'jamie.jpeg', 'matthew.jpeg', 'michael.png', 'ross.png', 'zack.png'];
 
     var pickedOn = [];
-
-
-    var items = [];
 
     $(document).ready(function () {
         var index;
         var student;
         var count = 0;
 
-        $("#ask").click(function () {
+        getStudents();
 
-            getStudents();
+        $("#ask").click(function () {
 
             if (count === students.length) {
                 alert("You have picked on everyone today.  Please refresh the page to start again.");
@@ -26,17 +23,17 @@
             while (pickedOnStudentAlreadyFlag === true && count < students.length) {
                 index = generateStudentIndex();
                 student = students[index];
-                console.log("student = " + student);
-                pickedOnStudentAlreadyFlag = pickedOnStudentAlready(student);
+                console.log("student = " + student.name);
+                pickedOnStudentAlreadyFlag = pickedOnStudentAlready(student.name);
 
             }
 
             console.log(pickedOn);
 
-            var image = images[index];
-            $(".student-name").text(student);
+            //var image = images[index].image;
+            $(".student-name").text(student.name);
             $(".student-name").removeClass('hide');
-            $(".img-circle").attr("src", "./images/" + image);
+            $(".img-circle").attr("src", "./images/" + student.image);
             $(".student-image").removeClass('hide');
             $(".thank-you").removeClass('hide');
         });
@@ -47,12 +44,12 @@
         }
 
         function pickedOnStudentAlready(student) {
-            if (pickedOn.includes(student)) {
+            if (pickedOn.includes(student.name)) {
                 console.log("found student!");
                 return true;
             } else {
                 console.log("didn't find student");
-                pickedOn.push(student);
+                pickedOn.push(student.name);
                 count++;
                 return false;
             }
@@ -69,7 +66,7 @@
                 //     "class": "my-new-list",
                 //     html: items.join("")
                 // }).appendTo("body");
-                items = data;
+                students = data.students;
             });
         }
 
