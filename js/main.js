8fb4afb966d9b72de0eb964f70041ff115c73578
Ed Orsini <edorsini@mit.edu>
2017-07-18 09:27:20 -0400

    Working on teams - responsive

--- js/main.js
+++ js/main.js
@@ -159,7 +159,7 @@ $(document).ready(function() {
      */
     function populateAbsentModal() {
         for (var i = 0; i < students.length; i++) {
-            $(".modal-body").append("<div class='absent-student'><li class='list-group-item'><input type='text' value='" + students[i].name + "'>" + students[i].name + "</li></div>");
+            $(".modal-body").append("<div class='absent-student'><li class='list-group-item'><input type='checkbox'>" + students[i].name + "</li></div>");
         }
     }
 
